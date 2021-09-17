9bd7567e42c5e94705e611cb6c30c0158ef091fb
nuryadincjr <nuryadin.cjr@gmail.com>
2021-09-17 20:31:54 +0700

    use view binding

--- app/src/main/java/com/nuryadincjr/counterhomework/MainActivity.java
+++ app/src/main/java/com/nuryadincjr/counterhomework/MainActivity.java
@@ -1,50 +1,38 @@
 package com.nuryadincjr.counterhomework;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import com.nuryadincjr.counterhomework.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private TextView viewConter;
-    private EditText editText;
+    ActivityMainBinding binding;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-        viewConter = (TextView) findViewById(R.id.txtCounter);
-        editText = (EditText) findViewById(R.id.inpEditText);
-        button = (Button) findViewById(R.id.btnCount);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int count = Integer.parseInt(viewConter.getText().toString());
-                count += 1;
+        binding.btnCount.setOnClickListener(view -> {
+            count = Integer.parseInt(binding.txtCounter.getText().toString());
+            count += 1;
 
-                viewConter.setText(String.valueOf(count));
-            }
+            binding.txtCounter.setText(String.valueOf(count));
         });
 
         //01 retrieve the saved Bundle from onCreate
         if (savedInstanceState != null) {
             String count = savedInstanceState.getString("count");
 
-            if (viewConter != null)
-                viewConter.setText(count);
-
+            binding.txtCounter.setText(count);
         }
     }
 
@@ -53,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
 
         Toast.makeText(this, "Saved count is " +
-                viewConter.getText().toString(), Toast.LENGTH_SHORT).show();
+                binding.txtCounter.getText().toString(), Toast.LENGTH_SHORT).show();
 
-        outState.putString("count", String.valueOf(viewConter.getText()));
+        outState.putString("count", String.valueOf(binding.txtCounter.getText()));
     }
 
     //02 retrieve the saved Bundle, called after onStart()
@@ -65,42 +53,7 @@ public class MainActivity extends AppCompatActivity {
 //        if (savedInstanceState != null) {
 //            String count = savedInstanceState.getString("count");
 //
-//            if (count != null)
-//                viewConter.setText(count);
-//
+//            binding.txtCounter.setText(count);
 //        }
 //    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
-    }
-
-
 }
